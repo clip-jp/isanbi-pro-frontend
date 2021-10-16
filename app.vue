b97550bd4262eb99c75d81f3ba21bd1c1c95f0e6
Victor Toh <1786093+zeppytoh@users.noreply.github.com>
2021-10-16 22:34:39 +0900

    Change home page text

--- app.vue
+++ app.vue
@@ -3,10 +3,10 @@
   <div class="text-center hero-content">
     <div class="max-w-md">
       <h1 class="mb-5 text-5xl font-bold">
-            Hello there
+            愛賛美へようこそ
           </h1> 
       <p class="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+            Welcome to iSanbi
           </p> 
       <button class="btn btn-primary">Get Started</button>
     </div>
