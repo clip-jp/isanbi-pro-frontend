1437cd66bb9022b033e0974530071465e84d7c79
Victor Toh <1786093+zeppytoh@users.noreply.github.com>
2021-10-17 21:15:50 +0900

    Fix output directory for deploying to gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,5 @@ import { defineNuxtConfig } from 'nuxt3'
 export default defineNuxtConfig({
   buildModules: [
     'nuxt-windicss',
-  ],
-  target: 'static',
-  router: {
-    base: '/isanbi-pro-frontend/'
-  }
+  ]
 })
