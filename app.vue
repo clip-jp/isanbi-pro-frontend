de3f596ae1917a595373e361182070bfb09fe919
Victor Toh <1786093+zeppytoh@users.noreply.github.com>
2021-10-17 20:37:36 +0900

    Add default route

--- app.vue
+++ app.vue
@@ -1,15 +1,2 @@
 <template>
-<div class="hero min-h-screen bg-base-200">
-  <div class="text-center hero-content">
-    <div class="max-w-md">
-      <h1 class="mb-5 text-5xl font-bold">
-            愛賛美へようこそ
-          </h1> 
-      <p class="mb-5">
-            Welcome to iSanbi
-          </p> 
-      <button class="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
 </template>
